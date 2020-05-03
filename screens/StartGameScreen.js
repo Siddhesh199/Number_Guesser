7f96fc21c61884b901cc9e1418904f7fd4a13f0b
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-05-03 23:04:10 +0530

    Created Input component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 import Card from "../components/Card";
+import Input from "../components/Input";
 import Colors from "../constants/colors";
 
 const StartGameScreen = (props) => {
@@ -10,10 +11,21 @@ const StartGameScreen = (props) => {
       <Text>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text style={styles.title}>Select a Number</Text>
-        <TextInput />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          maxLength={2}
+        />
         <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.accent} /></View>
-          <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary} /></View>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+          </View>
         </View>
       </Card>
     </View>
@@ -31,9 +43,9 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   inputContainer: {
-      width: 300,
-      maxWidth: '80%',
-      alignItems: "center",
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
   },
   buttonContainer: {
     flexDirection: "row",
@@ -42,8 +54,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   button: {
-      width: 100,
-  }
+    width: 100,
+  },
+  input: {
+    width: 50,
+    textAlign: "center",
+  },
 });
 
 export default StartGameScreen;
